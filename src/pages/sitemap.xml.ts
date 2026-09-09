bfa015ce9418417cc1ec214a83cd091a957b6cac
Joy | JoyLab <ohbeopseok@gmail.com>
2026-09-09 12:38:55 +0900

    seo: add dynamic sitemap

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,21 @@
+import { getCollection } from 'astro:content';
+
+export const prerender = true;
+
+export async function GET({ site }: { site: URL }) {
+  const origin = site?.origin ?? 'https://joylab-publishing-os.ohbeopseok.workers.dev';
+  const articles = await getCollection('articles');
+
+  const urls = [
+    `${origin}/`,
+    ...articles.map((article) => `${origin}/articles/${article.id.replace(/\.mdx?$/, '')}`)
+  ];
+
+  const body = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${urls
+    .map((url) => `  <url><loc>${url}</loc></url>`)
+    .join('\n')}\n</urlset>`;
+
+  return new Response(body, {
+    headers: { 'Content-Type': 'application/xml; charset=utf-8' }
+  });
+}
